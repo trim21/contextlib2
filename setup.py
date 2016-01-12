502124ce606b3f8da01c072a5f0673cfa8bd308c
Nick Coghlan <@ncoghlan>
2016-01-12 16:42:24 +1000

    Adjust packaging for renamed README

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     py_modules=['contextlib2'],
     license='PSF License',
     description='Backports and enhancements for the contextlib module',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     author='Nick Coghlan',
     author_email='ncoghlan@gmail.com',
     url='http://contextlib2.readthedocs.org'
