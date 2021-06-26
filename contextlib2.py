cf0cece8370d09e86bcb05941a34e8b73e545e7e
Nick Coghlan <ncoghlan@gmail.com>
2021-06-26 16:40:28 +1000

    Add back refresh_cm

--- contextlib2.py
+++ contextlib2.py
@@ -63,6 +63,23 @@ class AbstractAsyncContextManager(abc.ABC):
 class ContextDecorator(object):
     "A base class or mixin that enables context managers to work as decorators."
 
+    def refresh_cm(self):
+        """Returns the context manager used to actually wrap the call to the
+        decorated function.
+
+        The default implementation just returns *self*.
+
+        Overriding this method allows otherwise one-shot context managers
+        like _GeneratorContextManager to support use as decorators via
+        implicit recreation.
+
+        DEPRECATED: refresh_cm was never added to the standard library's
+                    ContextDecorator API
+        """
+        warnings.warn("refresh_cm was never added to the standard library",
+                      DeprecationWarning)
+        return self._recreate_cm()
+
     def _recreate_cm(self):
         """Return a recreated instance of self.
 
